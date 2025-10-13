03368c70e4a91c7020e797d638a0779438d11550
Marcos <marcosgarba2006@gmail.com>
2025-10-13 12:18:00 +0200

    dado hecho

--- partida/Dado.java
+++ partida/Dado.java
@@ -1,15 +1,33 @@
 package partida;
 
-
 public class Dado {
-    //El dado solo tiene un atributo en nuestro caso: su valor.
-    private int valor;
+    // Atributos para los valores de los dos dados
+    private int valor1;
+    private int valor2;
 
-    //Metodo para simular lanzamiento de un dado: devolverá un valor aleatorio entre 1 y 6.
-    public int hacerTirada() {
-        valor = (int) (Math.random() * 6) + 1;
-        return valor;
+    // Método para lanzar ambos dados
+    //se crean 2 valores para los 2 dados y se hace random
+    public int[] hacerTirada() {
+        valor1 = (int) (Math.random() * 6) + 1;
+        valor2 = (int) (Math.random() * 6) + 1;
+        return new int[]{valor1, valor2};
     }
 
+    //Comprobar si son dobles
+    public boolean sonDobles() {
+        return valor1 == valor2;
+    }
 
+    //Obtener el total
+    public int suma() {
+        return valor1 + valor2;
+    }
+
+    //Getters de cada valor
+    public int getValor1() {
+        return valor1;
+    }
+    public int getValor2() {
+        return valor2;
+    }
 }
